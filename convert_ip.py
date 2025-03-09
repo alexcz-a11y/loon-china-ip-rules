68c9da7c7e43fb068c249129fa0bacae18817fc4
alexcz-a11y <alexcopd@gmail.com>
2025-03-09 17:21:48 +0800

    Update convert_ip.py

--- convert_ip.py
+++ convert_ip.py
@@ -3,4 +3,5 @@ with open('chn_ip.txt', 'r') as f:
 with open('chn_ip_loon.txt', 'w') as f:
     for line in lines:
         if line.strip():  # 忽略空行
+            # 假设 chn_ip.txt 已经是 CIDR 格式，如 1.0.1.0/24
             f.write(f'IP-CIDR,{line.strip()},DIRECT\n')
